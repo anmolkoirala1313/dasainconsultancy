3d2f39c78b343b6f8e4644f93da704669c832ff0
anmolko <anmol@canosoft.com.np>
2023-11-08 00:22:23 +0545

    headers update

--- resources/views/frontend/partials/seo_header.blade.php
+++ resources/views/frontend/partials/seo_header.blade.php
@@ -62,9 +62,9 @@
 <div class="custom-cursor__cursor"></div>
 <div class="custom-cursor__cursor-two"></div>
 
-<div class="preloader">
-    <div class="preloader__image"></div>
-</div>
+{{--<div class="preloader">--}}
+{{--    <div class="preloader__image"></div>--}}
+{{--</div>--}}
 <!-- /.preloader -->
 
 
